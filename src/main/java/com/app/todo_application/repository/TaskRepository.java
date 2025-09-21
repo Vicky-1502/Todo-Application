d66a5885ed612f1cc0e127ee1fb22c66b1e8c0e0
Vicky Jeswani <jeswanivicky@gmail.com>
2025-09-21 14:28:03 +0530

    Initial Commit

---
+++ src/main/java/com/app/todo_application/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.app.todo_application.repository;
+
+import com.app.todo_application.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
